044e89ddcb7205f4915c9ccc13237439dfeeb15c
Manoj Chhatralia <mchhatralia@hotmail.com>
2020-08-21 21:30:23 +0100

    Completed Exercise002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,32 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich['fillings'];
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if (person.city === "Manchester") { return true; }
+  else return false;
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  
+  const maxPeoplePerBus = 40;
+  var num = Math.floor(people / maxPeoplePerBus );
+  if (people % maxPeoplePerBus != 0) { num++; }
+  return num;
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  return arr.filter(function(x){ return x === "sheep"; }).length;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+
+  return (person.address.city === "Manchester" && person.address.postCode.charAt(0) === 'M');
 }
 
 module.exports = {
