a5d5d44fb14f3b433e22d706af4213b8a7c294dc
Manoj Chhatralia <mchhatralia@hotmail.com>
2020-08-22 11:39:06 +0100

    Refactored Exercise003

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -5,21 +5,23 @@ function getSquares(nums) {
 
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  return words.slice(1).reduce(function(p,c) { return p + c[0].charAt(0).toUpperCase() + c.slice(1)},
-                               words[0].charAt(0).toLowerCase() + words[0].slice(1));
+  return words.slice(1).
+    reduce((prevWord, currWord) => prevWord + currWord[0].charAt(0).toUpperCase() + currWord.slice(1),
+      words[0].charAt(0).toLowerCase() + words[0].slice(1)
+    );
 }
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  let arr1 = people.filter(x => x.subjects.length > 0 );
-  totalSubjects = arr1.map(obj => obj.subjects ).reduce( function(p,c) {return p + c.length} ,0);
-  return totalSubjects;
+  return people.filter(person => person.subjects.length > 0).
+    map(personSubjectObj => personSubjectObj.subjects).
+    reduce((prevSubjects, currSubjects) => prevSubjects + currSubjects.length, 0);
 }
 
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  return (menu.filter(menuItem => menuItem.ingredients.includes(ingredient) ).length > 0);
+  return (menu.filter(menuItem => menuItem.ingredients.includes(ingredient)).length > 0);
 }
 
 function duplicateNumbers(arr1, arr2) {
