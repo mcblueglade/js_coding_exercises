71067d1e1fc5f1e3bc3900266a5740e8156346a4
mcblueglade <mchhatralia@hotmail.com>
2020-09-13 13:51:14 +0100

    Added TVRemote to as Exercise008 and removed from Exercise007

--- challenges/exercise008.js
+++ challenges/exercise008.js
@@ -6,39 +6,9 @@ var tvRemote = function (word) {
     ["p", "q", "r", "s", "t", ".", "@", "0"],
     ["u", "v", "w", "x", "y", "z", "-", "/"],
   ];
+
   let firstLetter = "a";
-  //let totalClicks = 0;
   const findLocationOfLetter = (letter) => {
-    //let r = 0;
-    //let c = 0;
-    //let found = false;
-    // for (r = 0; r < map.length; r++) {
-    //   for (c = 0; c < map[r].length; c++) {
-    //     if (map[r][c] == letter) {
-    //       found = true;
-    //       break;
-    //     }
-    //     if (found) break;
-    //   }
-    //   if (found) break;
-    // }
-    // return [r, c];
-
-    /*
-    return map.reduce((pCoordA, r, i) => {
-      if (pCoordA === null) {
-        return r.reduce((pCoordB, c, j) => {
-          if (pCoordB === null) {
-            //console.log("C=", c, "Letter=", letter, "[i,j]=", i, j);
-            pCoordB = pCoordB === null ? (c === letter ? [i, j] : null) : null;
-          }
-          return pCoordB;
-        }, null);
-      }
-      return pCoordA;
-    }, null);
-    */
-
     return map.reduce(
       (pCoordA, r, i) =>
         pCoordA === null
@@ -55,30 +25,11 @@ var tvRemote = function (word) {
     );
   };
 
-  /*  
-  let letters = word.toString().split("");
-  letters.forEach((letter) => {
-    let firstCoords = findLocationOfLetter(firstLetter);
-    let secondCoords = findLocationOfLetter(letter);
-    firstLetter = letter;
-    totalClicks =
-      totalClicks +
-      (Math.abs(secondCoords[0] - firstCoords[0]) +
-        Math.abs(secondCoords[1] - firstCoords[1]) +
-        1);
-    //console.log(`fLet ${firstLetter} sLet ${letter} s0 = ${secondCoords[0]} f0 = ${firstCoords[0]} s1 = ${secondCoords[1]} f1 = ${firstCoords[1]}` );
-    console.log(
-      `fLetMC*** ${firstLetter}, ${letter} ${secondCoords[0]}${secondCoords[1]} ${firstCoords[0]}${firstCoords[1]} = ${totalClicks}`
-    );
-  });
-  return totalClicks;
-  */
-
   let firstCoords = findLocationOfLetter(firstLetter);
 
   return word.split("").reduce((totalClicks, c) => {
     let secondCoords = findLocationOfLetter(c);
-    
+
     totalClicks +=
       Math.abs(secondCoords[0] - firstCoords[0]) +
       Math.abs(secondCoords[1] - firstCoords[1]) +
